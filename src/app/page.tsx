aa8089c389738127b6012e67c0ce04db6b5f9cad
Muhammadkashif5 <muhammad.kashif7016992@gmail.com>
2024-10-26 21:39:36 +0500

    Push the project on GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col items-center justify-center min-h-screen pt-8 pb-8 space-y-10 -mt-20 w-[990px] m-auto">
+      {/* Heading Div */}
+      <div className="flex flex-col items-center text-center w-[550px] p-12 space-y-4">
+        <h5 className="text-white text-[16px] leading-[24px] tracking-[0.1px] font-montserrat mt-24">
+          Welcome
+        </h5>
+        <h1 className="text-white text-[58px] leading-[80px] tracking-[0.2px] pt-3 font-montserrat">
+          Selling on the internet like a pro
+        </h1>
+        <h4 className="text-white text-[20px] leading-[30px] tracking-[0.2px] pt-3 font-montserrat">
+          We know how large objects will act, but things on a small scale just do not act that way.
+        </h4>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {/* Button Div */}
+        <div className="flex space-x-2 pt-4">
+          <button className="rounded-[5px] px-[25px] py-2 bg-[#23A6F0] text-white text-[14px] leading-[22px] tracking-[0.2px] font-montserrat">
+            Get Quote Now
+          </button>
+          <button className="rounded-[5px] px-[25px] py-2 border border-[#23A6F0] text-white text-[14px] leading-[22px] tracking-[0.2px] font-montserrat">
+            Learn More
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="grid grid-cols-3 gap-5 w-full  mt-8">
+  {/* First Column */}
+  <div className="bg-[#FFFFFF] rounded w-80 h-72 pt-8 px-12">
+    <div className="flex flex-col items-start">
+      <div className="w-[70px] h-[76px] bg-[#FFDCD1] rounded-lg mb-4 "></div>
+      <h5 className="text-[#252B42] text-[16px] leading-[24px] tracking-[0.1px] font-montserrat">
+        Training Courses
+      </h5>
+      <div className="w-[50px] h-[2px] bg-[#E74040] mt-4"></div>
+      <p className="text-[#737373] text-[14px] leading-[20px] tracking-[0.2px] pt-5 font-montserrat">
+        The gradual accumulation of information about atomic and small-scale behaviour...
+      </p>
     </div>
-  );
+  </div>
+
+  {/* Second Column */}
+  <div className="bg-[#FFFFFF] p-4 rounded w-80 h-72 pt-8 px-12">
+    <div className="flex flex-col items-start">
+      <div className="w-[70px] h-[76px] bg-[#B9EAA8] rounded-lg mb-4"></div>
+      <h5 className="text-[#252B42] text-[16px] leading-[24px] tracking-[0.1px] font-montserrat">
+        2,769 online courses
+      </h5>
+      <div className="w-[50px] h-[2px] bg-[#E74040] mt-4"></div>
+      <p className="text-[#737373] text-[14px] leading-[20px] tracking-[0.2px] pt-5 font-montserrat">
+        The gradual accumulation of information about atomic and small-scale behaviour...
+      </p>
+    </div>
+  </div>
+
+  {/* Third Column */}
+  <div className="bg-[#23A6F0] p-4 rounded w-80 h-72 pt-8 px-12 ">
+    <div className="flex flex-col items-start">
+      <div className="w-[70px] h-[76px] bg-[#FFFFFF] rounded-lg"></div>
+      <h5 className="text-[#FFFFFF] text-[16px] leading-[24px] tracking-[0.1px] pt-4 font-montserrat">
+        Training Courses
+      </h5>
+      <div className="w-[50px] h-[2px] bg-[#FFFFFF] mt-4 "></div>
+      <p className="text-[#FFFFFF] text-[14px] leading-[20px] tracking-[0.2px] pt-5 font-montserrat">
+        The gradual accumulation of information about atomic and small-scale behaviour...
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+)
 }
